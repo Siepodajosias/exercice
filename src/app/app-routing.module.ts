0542f6fd7f9196eeb9e19e550dd12c8485e3c078
sie-poda <josias.sie@eburtis.ci>
2023-01-12 13:46:09 +0100

    mise a jour

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'accueil', loadChildren: ()=> import('./product/product.module').then((m)=> m.ProductModule)},
+  {path:'', loadChildren: ()=> import('./product/product.module').then((m)=> m.ProductModule)},
+  {path:'**', loadChildren: ()=> import('./product/product.module').then((m)=> m.ProductModule)}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
